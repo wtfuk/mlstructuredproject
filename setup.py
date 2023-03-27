502ff476e4bfd95a9d3cea1f427cb8535c9ad31f
Sagar Yadav <5121817+sagarishere@users.noreply.github.com>
2023-03-27 06:43:44 +0300

    2 nbs before python native

--- setup.py
+++ setup.py
@@ -1,21 +1,35 @@
+import pkg_resources
+from setuptools import setup
 from setuptools import setup, find_packages
 
 
 def get_requirements(path):
     with open(path) as f:
-        reqlist = [i for i in f.read().splitlines() if not i.startswith('#') and not i.startswith('-')]
+        reqlist = [i for i in f.read().splitlines() if not i.startswith(
+            '#') and not i.startswith('-')]
     return reqlist
 
 
-setup(
-    name = "genericmlboilerplate",
-    version="0.1",
-    packages = find_packages(),
-    install_requires = get_requirements('requirements.txt'),
-    # metadata for upload to PyPI
-    author = "Sagar Yadav",
-    author_email = "sagar@sagaryadav.com",
-    description = "Fast ML model develoment boilerplate",
-    license = "PSF",
-    keywords = "machine learning boilerplate data science",
-)
+# Check if package is already installed and of the same version or below
+try:
+    installed_version = pkg_resources.get_distribution("mypackage").version
+    if pkg_resources.parse_version(installed_version) >= pkg_resources.parse_version("1.0.0"):
+        print("mypackage is already installed and of the same version or below")
+    else:
+        print("mypackage is installed but not of the same version or below, installing with -e .")
+        setup(name='mypackage', packages=['mypackage'], install_requires=[
+        ], extras_require={}, dependency_links=['-e .'])
+except pkg_resources.DistributionNotFound:
+    print("mypackage is not installed, installing with -e .")
+    setup(name='genericmlboilerplate',
+          version='0.1',
+          packages=['genericmlboilerplate'],
+          install_requires=get_requirements('requirements.txt'),
+          # metadata for upload to PyPI
+          author="Sagar Yadav",
+          author_email="sagar@sagaryadav.com",
+          description="Fast ML model develoment boilerplate",
+          license="PSF",
+          keywords="machine learning boilerplate data science",
+          dependency_links=['-e .']
+          )
